12b21c6a63f0443e3aac73a3ce55c080463c77a0
Noorul Ameen <noorulamean146@gmail.com>
2024-03-09 22:17:08 +0530

    minor modifications

--- main.py
+++ main.py
@@ -14,11 +14,19 @@ def saver(parameters_: dict = None, results: dict = None, description=None) -> N
         pass
 
     for dictionary in dict_list:
+        total_score = 0
         if dictionary == None:
             continue
         date_time_stamp = datetime.datetime.now().strftime("%d_%b_%H_%M_%S")
         with open(f'{ARTIFACT_PATH}{date_time_stamp}.json', 'w') as file:
             json.dump(dictionary, file)
+
+        for key, value in dictionary.items():
+            if type(value) != float:
+                break
+            total_score += value
+        total_score = total_score/7
         with open(f'{ARTIFACT_PATH}description.txt', 'a') as file:
-            file.write(f'{date_time_stamp}:- {description}\n')
+            file.write(
+                f'{date_time_stamp}:- score: {total_score}, {description}\n')
         time.sleep(4)
